4c3c73e00f1d1c35abe45ed34c9d95448a002107
Chhanda Saha <chhandasaha98@gmail.com>
2022-07-13 17:52:37 +0530

    get eth&token balance from api

--- transaction.controller.js
+++ transaction.controller.js
@@ -52,7 +52,6 @@ const address = '0xe634d62E3c78412165DAcc9547BC0CbFA239a54B';
 const privateKey = 'e30495872ee88d814739adeaf0fdd8841675ad7969815c8db4de3e38dc7849f5';
 const infuraUrl = 'https://rinkeby.infura.io/v3/ed40d244d07b4beba6246525ad60917b'; 
 
-//const init1 = async () => {
   exports.transfer = async function(req, res) {
     console.log(req.body);
     const web3 = new Web3(infuraUrl);
@@ -88,11 +87,62 @@ const infuraUrl = 'https://rinkeby.infura.io/v3/ed40d244d07b4beba6246525ad60917b
       }  
 }
 
-// const testnet = 'https://rinkeby.infura.io';
-// const web3 = new Web3(new Web3.providers.HttpProvider(testnet));
+//to get eth balance in rinkeby
+// exports.checkBalance = async function(req, res){
 
-// exports.checkBalance = async function(req, res) {
-//   var balance = web3.eth.getBalance(address);
-//   balance = web3.utils.toDecimal(balance); 
-//   console.log(balance);
+// const web3 = new Web3(new Web3.providers.HttpProvider("https://rinkeby.infura.io/v3/ed40d244d07b4beba6246525ad60917b"))
+
+// const getBal = await web3.eth.getBalance(address, function(err, result) {
+//   if (err) {
+//     console.log(err)
+//   } else {
+//     console.log(web3.utils.fromWei(result, "ether") + " ETH")
+//   }
+// }); 
 // }
+
+//to get token balance in rinkeby
+exports.checkTknBalance = async function(req, res){
+const web3 = new Web3(infuraUrl)
+
+//let tokenAddress = "0x20fe562d797a42dcb3399062ae9546cd06f63280";
+
+let tokenAddress = "0x54cd8339239d6428076fD36129c5E9aB92D936aD";
+
+
+// The minimum ABI to get ERC20 Token balance
+let minABI = [
+  // balanceOf
+  {
+    "constant":true,
+    "inputs":[{"name":"_owner","type":"address"}],
+    "name":"balanceOf",
+    "outputs":[{"name":"balance","type":"uint256"}],
+    "type":"function"
+  },
+  // decimals
+  {
+    "constant":true,
+    "inputs":[],
+    "name":"decimals",
+    "outputs":[{"name":"","type":"uint8"}],
+    "type":"function"
+  }
+];
+
+let contract = new web3.eth.Contract(minABI,tokenAddress);
+async function getBalance() {
+  balance = await contract.methods.balanceOf(address).call();
+  return balance;
+}
+getBalance().then(function (balance) {
+  result = {
+    "balanace" : balance,
+    "token": "Zazzle"
+  }
+  console.log(result);
+  res.send(result);
+});
+
+//console.log(getBalance());
+}
--- transaction.route.js
+++ transaction.route.js
@@ -10,6 +10,7 @@ const transaction_controller = require('./transaction.controller');
 router.post('/transaction', transaction_controller.transfer);
 
 //router.post('/transaction_status', transaction_controller.transactionStatus);
-router.post('/account_balance', transaction_controller.checkBalance);
-
+//router.post('/account_balance', transaction_controller.checkBalance);
+router.post('/account_tknBalance', transaction_controller.checkTknBalance);
+ 
 module.exports = router;
